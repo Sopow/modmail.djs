d91503575d7a4e896d7c28e9a9a25e59dd63abb5
Sopow <88051510+Sopow@users.noreply.github.com>
2022-03-18 22:37:31 +0100

    finished

--- test/index.js
+++ test/index.js
@@ -10,10 +10,10 @@ const client = new Client({
     partials: ['MESSAGE', 'CHANNEL', 'REACTION']
 })
 const manager = new Manager(client, {
-    guild: '925331433831170058',
-    category: 'Tickets',
-    prefix: '!',
-    role: '928588994512572496'
+    guild: '',
+    category: '',
+    prefix: '',
+    role: ''
 })
 
 client.on('ready', () => {
@@ -21,4 +21,4 @@ client.on('ready', () => {
     manager.setModmail();
 })
 
-client.login('OTM2MzM3NzQ5MzUzNTk0OTIw.YfLuew.8o0kULZ0lf9FTueKvdIkPpUoGHs')
+client.login('')
