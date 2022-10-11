b5f3a80a880ec2b82279f256b7dd89aa0c5bed17
Idris <78701338+Idris1401@users.noreply.github.com>
2022-10-11 13:09:16 +0200

    fix(test): tests were outdated (#11)

--- test/index.js
+++ test/index.js
@@ -1,11 +1,14 @@
 const { Manager } = require("modmail.djs");
-const { Client, Partials } = require("discord.js")
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
 
-const client = new Client({ intents: 131071, partials: [Partials.Message, Partials.Channel, Partials.Reaction] })
+const client = new Client({
+  intents: Object.keys(GatewayIntentBits),
+  partials: [Partials.Message, Partials.Channel, Partials.Reaction],
+});
 
-client.on("ready", () => {
-  console.log("Ready!")
-  new Manager(client, { guild: "", categoryId: "", role: "" }).setModmail();
-})
+client.once("ready", () => {
+  console.log("Ready!");
+  new Manager(client, { guildId: "", categoryId: "", role: "" }).setModmail();
+});
 
-client.login("")
+client.login("");
