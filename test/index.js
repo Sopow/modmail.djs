5aa061cdc841e00ce81e209edb9d60288082fc66
Idris <78701338+Idris1401@users.noreply.github.com>
2022-10-14 09:06:57 +0200

    chore: update test

--- test/index.js
+++ test/index.js
@@ -1,14 +1,20 @@
-const { Manager } = require("modmail.djs");
-const { Client, GatewayIntentBits, Partials } = require("discord.js");
+const { Manager } = require('modmail.djs');
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
 
 const client = new Client({
-  intents: Object.keys(GatewayIntentBits),
-  partials: [Partials.Message, Partials.Channel, Partials.Reaction],
+    intents: Object.keys(GatewayIntentBits), // all intents
+    partials: [Partials.Message, Partials.Channel, Partials.Reaction]
 });
 
-client.once("ready", () => {
-  console.log("Ready!");
-  new Manager(client, { guildId: "", categoryId: "", role: "" }).setModmail();
+const manager = new Manager(client, {
+    guildId: '',
+    categoryId: '',
+    role: ''
 });
 
-client.login("");
+client.once('ready', () => {
+    console.log('Ready!');
+    manager.setModmail();
+})
+
+client.login('');
